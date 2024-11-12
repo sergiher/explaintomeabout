c96d68dee5fed28977c1186b958e096bc26e1f8d
sergio <sergiher@gmail.com>
2024-11-12 19:52:41 +0100

    ui

---
+++ frontend/app/components/slider.tsx
@@ -0,0 +1,70 @@
+import * as React from "react";
+import { styled } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import Slider from "@mui/material/Slider";
+import MuiInput from "@mui/material/Input";
+import UpdateIcon from "@mui/icons-material/Update";
+import Typography from "@mui/material/Typography";
+
+const Input = styled(MuiInput)`
+  width: 42px;
+`;
+
+export default function InputSlider() {
+  const [value, setValue] = React.useState(10);
+
+  const handleSliderChange = (event: Event, newValue: number | number[]) => {
+    setValue(newValue as number);
+  };
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(event.target.value === "" ? 0 : Number(event.target.value));
+  };
+
+  const handleBlur = () => {
+    if (value < 1) {
+      setValue(1);
+    } else if (value > 60) {
+      setValue(60);
+    }
+  };
+
+  return (
+    <Box sx={{ width: 306 }}>
+      <Grid container spacing={4}>
+        <Grid item>
+          <UpdateIcon />
+        </Grid>
+        <Grid item xs>
+          <Slider
+            value={typeof value === "number" ? value : 0}
+            onChange={handleSliderChange}
+            aria-labelledby="input-slider"
+            min={1}
+            max={60}
+          />
+        </Grid>
+        <Grid item style={{ display: "inline-flex" }}>
+          <Input
+            value={value}
+            size="small"
+            onChange={handleInputChange}
+            onBlur={handleBlur}
+            inputProps={{
+              step: 1,
+              min: 1,
+              max: 60,
+              type: "number",
+              "aria-labelledby": "input-slider",
+            }}
+          />{" "}
+          <Typography>
+            minute
+            <span style={value === 1 ? { color: "white" } : {}}>s</span>
+          </Typography>
+        </Grid>
+      </Grid>
+    </Box>
+  );
+}
