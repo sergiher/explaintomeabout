bf67697555f8c84aec65ccc5923553841171e491
sergio <sergiher@gmail.com>
2024-11-13 16:53:48 +0100

    specifying the .env file to use

--- backend/gemini_class.py
+++ backend/gemini_class.py
@@ -1,11 +1,15 @@
 import os
+from os.path import dirname, join
 
 import google.generativeai as genai  # type: ignore
+from dotenv import load_dotenv
 
 
 class myGeminiAI:
     def __init__(self, model_name):
         self.model_name = model_name
+        dotenv_path = join(dirname(__file__), ".env")
+        load_dotenv(dotenv_path)
         self.api_key = os.getenv("GOOGLE_API_KEY")
         genai.configure(api_key=self.api_key)
         self.model = genai.GenerativeModel(self.model_name)
